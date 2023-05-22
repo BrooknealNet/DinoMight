ac77ada8653ae351aba88b61bca3468a93316f63
Gerald Bruno <34822464+NephilimEyez@users.noreply.github.com>
2023-05-22 16:57:06 -0400

    added notes for what is needed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 // *** Globals ***
 
+// Probably several but I am not sure what yet
 
 // *** Constructors ***
 
@@ -15,12 +16,27 @@ function Dinosaur(name, wasSeen, fileExtension = 'png') {
     this.wasSeen = wasSeen;
 }
 
+// *** Local Storage ***
+
+// need two local storages. One for the dino memory game that is replaced every time they return to index and one for 
+// best score/time that is not replaced, but added on to. Maybe stores best 5 scores?
+
 // *** Helper Functions ***
 
+// random number generator that returns 1 to how ever many dinos we have
 
+// a function to make sure each dinosaur is only picked once. 
+
+// event listners for both buttons
+
+// event listeners for the dinosaurs they click on
 
 // *** Prototypes ***
 
+// gonna need a prototype to pick however many dinosaurs and display them on the page
 
+// a porototype to render all the dinosaurs on one page 
+
+// a prototype to track score
 
 // *** Executables ***
