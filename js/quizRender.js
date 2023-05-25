950b0250fd43974e0c5b84eb67f59e0e698db16e
Gerald Bruno <34822464+NephilimEyez@users.noreply.github.com>
2023-05-25 08:07:50 -0400

    cant get new window to work

--- js/quizRender.js
+++ js/quizRender.js
@@ -1,13 +1,10 @@
 // *** Globals ***
 
-let populated = false;
-
 // creating a new instance of the program
 let app = new AppState();
 
 // populating that instance with our local save
 app.loadItems();
-let dinoIndexArray = [];
 
 // *** DOCUMENT WINDOWS ***
 let dinoQuiz = document.getElementById('quiz-images');
@@ -16,13 +13,14 @@ function renderQuizImages(){
   for (let i = 0; i < app.allDinosaurs.length; i++) {
 
     // these assign variables the values from the dinosaur objects
-    let dinoImageQuiz = app.allDinosaurs[i].image
-    let dinoTitleQuiz = app.allDinosaurs[i].name
-    let dinoDescripQuiz = app.allDinosaurs[i].facts
+    let dinoImageQuiz = app.allDinosaurs[i].image;
+    let dinoTitleQuiz = app.allDinosaurs[i].name;
+    let dinoDescripQuiz = app.allDinosaurs[i].facts;
 
     // This creates a table row with the image of the dinosaur in it. It also sets the alt to the dinos name so the event listner knows which was clicked.
     let heroImageRowQuiz = document.createElement('tr');
-    heroImageRowQuiz.innerHTML = `<img src="${dinoImageQuiz}" alt="${dinoTitleQuiz}">`;
+    heroImageRowQuiz.classList.add('dino-card');
+    heroImageRowQuiz.innerHTML = `<img src="${dinoImageQuiz}" title="${dinoTitleQuiz}" alt="${dinoTitleQuiz}">`;
     dinoQuiz.appendChild(heroImageRowQuiz);
 
     // This creates a new row, everything below can be put on the quiz page
@@ -31,7 +29,7 @@ function renderQuizImages(){
             
     // this creates a table data cell with the Dinos name
     let dinoHeaderQuiz = document.createElement('td');
-    dinoHeaderQuiz.innerText = `Name: ${dinoTitleQuiz}`;
+    dinoHeaderQuiz.innerText = `${dinoTitleQuiz}`;
     dinoRowQuiz.appendChild(dinoHeaderQuiz);
             
     // This creates a table data cell with the dinos description
@@ -42,10 +40,18 @@ function renderQuizImages(){
 }
 
 function checkAnswer(event) {
-  let dinoClicked = event.target.name;
-  console.log(dinoClicked);
+  let dinoClicked = event.target.title;
+  for (let i = 0; i < app.allDinosaurs.length; i++) {
+    console.log(app.allDinosaurs[i]);
+    // if (app.allDinosaurs[i].name === dinoClicked && app.allDinosaurs[i].wasSeen === true) {
+    //   console.log('Correct!');
+    // } else {
+    //   console.log('wrong');
+    // }
+  }
 }
 
 renderQuizImages();
 
-dinoQuiz.addEventListener('click', checkAnswer);
+let test = document.getElementsByClassName('dino-card');
+test.addEventListener('click', checkAnswer);
